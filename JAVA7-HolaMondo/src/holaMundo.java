4f0e5918dd3a7767891349e9b42dce7d0187a979
TestSheepNZ <mike_talks@hotmail.com>
2016-08-29 21:26:07 +1200

    Done

---
+++ JAVA7-HolaMondo/src/holaMundo.java
@@ -0,0 +1,68 @@
+public class holaMundo {
+ 
+       public static void helloWorld (int langID)
+       {
+              if (langID == 1)
+              {
+                     System.out.println("Spanish selected");
+                     System.out.println("Hola Mundo!");
+              }
+              else if (langID == 2)
+              {
+                     System.out.println("German selected");
+                     System.out.println("Hallo Welt!");
+              }
+              else if (langID == 3)
+              {
+                     System.out.println("French selected");
+                     System.out.println("Bonjour le monde!");
+              }     
+              else
+              {
+                     System.out.println("English default");
+                     System.out.println("Hello world!");            
+              }
+       }
+      
+       public static void improvedHelloWorld (int langID)
+       {
+			  switch (langID)
+			  {
+			         case 1:
+			               System.out.println("Spanish selected");
+			               System.out.println("Hola Mundo!");
+			               break;
+			         case 2:
+			               System.out.println("German selected");
+			               System.out.println("Hallo Welt!");                    
+			         case 3:
+			               System.out.println("French selected");
+			               System.out.println("Bonjour le monde!");
+			               break;
+			         default:
+			               System.out.println("English default");
+			               System.out.println("Hello world!");     
+			  }
+       }
+      
+       public static void main(String[] args) {
+             
+              System.out.println("USING IF STATEMENTS");
+             
+              helloWorld(0);
+              helloWorld(1);
+              helloWorld(2);
+              helloWorld(3);
+              helloWorld(4);
+             
+              System.out.println();
+              System.out.println();
+              System.out.println("SWITCH STATEMENT");
+              improvedHelloWorld(0);
+              improvedHelloWorld(1);
+              improvedHelloWorld(2);
+              improvedHelloWorld(3);
+              improvedHelloWorld(4);
+       }
+      
+}
